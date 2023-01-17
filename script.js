51f61b3acab6b1029232691a1a39d242d63da677
Shawn <shawnaugustine01@gmail.com>
2023-01-17 15:19:29 +0000

    added delete button but not working yet

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const button = document.getElementById("enter");
 
 button.addEventListener("click", (event) => {
   console.log("The button has been clicked!");
-
-  //handleToDoListSubmit();
 });
 
 //Form
@@ -14,12 +12,14 @@ const handleToDoListSubmit = (event) => {
     event.preventDefault();
     
     const createdList = document.createElement("li");
-    
     createdList.textContent = event.target.textBox.value; 
     
+    const createdButton = document.createElement("button");
+    createdButton.appendChild(document.createTextNode("Delete"));
+
     const newItem = document.querySelector("#list");
-    
     newItem.appendChild(createdList);
+    newItem.appendChild(createdButton);
   }
 
   toDoList.addEventListener("submit", handleToDoListSubmit);
